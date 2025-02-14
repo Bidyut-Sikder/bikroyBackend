718fbc114e22b68e64d8afa085f722f8404df26b
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-02-14 16:19:56 +0600

    fix:checking vercel.json

--- app.js
+++ app.js
@@ -1,75 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const rateLimit = require("express-rate-limit");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require("helmet")
-const hpp = require('hpp');
-const rateLimit = require('express-rate-limit')
-const mongoose = require('mongoose')
-const path = require('path')
+const router = require("./src/Routes/api");
 
-const router = require('./src/Routes/api');
+const app = express();
 
-const app = express()
-
-
-//sequrity middleware 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+//sequrity middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(mongoSanitize());
-app.use(helmet())
+app.use(helmet());
 app.use(hpp());
 //request rate limit
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3000 // limit each IP to 100 requests per windowMs
+  max: 3000, // limit each IP to 100 requests per windowMs
 });
 
-app.use(limiter)
-
- 
- 
-
+app.use(limiter);
 
 // mongoose.connect('mongodb://localhost:27017/DemoBikroy')
-  mongoose.connect("mongodb+srv://bidyutsikder420:bidyutkumar@cluster0.mbahdsf.mongodb.net/DemoBikroy")
+mongoose
+  .connect(
+    "mongodb+srv://bidyutsikder420:bidyutkumar@cluster0.mbahdsf.mongodb.net/DemoBikroy"
+  )
   .then(() => {
-    console.log('connected to db')
+    console.log("connected to db");
   })
   .catch((e) => {
-    console.log(e)
-  })
+    console.log(e);
+  });
 
+app.use("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Welcome to the API",
+  });
+});
 
-
-//Routing  
-app.use('/api/v1', router)
-
+//Routing
+app.use("/api/v1", router);
 
 //introducing frontend to backend
-app.use(express.static("client/dist"))
+// app.use(express.static("client/dist"))
 
-app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
-})
-
-
-
+// app.use("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+// })
 
 //error handler here
 function errorHandler(err, req, res, next) {
-  console.log("err")
-  res.status(500)
-  res.render('error', { error: err })
+  console.log("err");
+  res.status(500);
+  res.render("error", { error: err });
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-
-module.exports = app
+module.exports = app;
